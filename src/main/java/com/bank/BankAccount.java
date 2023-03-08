728726580da71f1c4400cb035567a7aa4590eebd
Anna Axelsson <a.axelsson51@gmail.com>
2023-03-08 17:24:17 +0100

    Add bank account class

---
+++ src/main/java/com/bank/BankAccount.java
@@ -0,0 +1,57 @@
+package com.bank;
+
+public class BankAccount {
+    double balance;
+    double prevTrans;
+    String customerName;
+    String customerId;
+
+    BankAccount(String customerName,String customerId){
+        this.customerName=customerName;
+        this.customerId=customerId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getPrevTrans() {
+        return prevTrans;
+    }
+
+    public void setPrevTrans(double prevTrans) {
+        this.prevTrans = prevTrans;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    void getPreviousTrans(){
+        if(prevTrans>0){
+            System.out.println("Deposited: "+prevTrans);
+        }
+        else if(prevTrans<0){
+            System.out.println("Withdrawn: "+Math.abs(prevTrans));
+        }
+        else{
+            System.out.println("No transaction occured");
+        }
+    }
+}
